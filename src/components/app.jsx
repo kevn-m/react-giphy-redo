3a1d74e76589bc7ed51252974c20cc1ea22821af
kevn-m <south.kay@gmail.com>
2022-01-06 10:04:11 +1100

    added search bar

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,16 @@
-import React from "react"
+import React, { Component } from "react"
+import SearchBar from "./search_bar"
 
 const App = () => {
-  return <div>Hello world</div>
+  return (
+    <div>
+      <div className="left-scene">
+        <SearchBar />
+        <div className="selected-gif"></div>
+      </div>
+      <div className="right-scene"></div>
+    </div>
+  )
 }
 
 export default App
