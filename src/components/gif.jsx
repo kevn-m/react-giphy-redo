fed87ac00cae8222bc4b076c738a7b0739f62547
kevn-m <south.kay@gmail.com>
2022-01-06 10:15:13 +1100

    added placeholder gif

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,7 +1,13 @@
 import React from "react"
 
 const gif = () => {
-  return <div></div>
+  return (
+    <img
+      className="gif"
+      src="https://media.giphy.com/media/3NtY188QaxDdC/giphy.gif"
+      alt=""
+    />
+  )
 }
 
 export default gif
