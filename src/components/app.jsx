45994636aae492abbf2ad7cd0247c99c53faeb6f
kevn-m <south.kay@gmail.com>
2022-01-06 10:09:10 +1100

    added gif component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react"
 import SearchBar from "./search_bar"
+import Gif from "./gif"
 
 const App = () => {
   return (
     <div>
       <div className="left-scene">
         <SearchBar />
-        <div className="selected-gif"></div>
+        <div className="selected-gif">
+          <Gif />
+        </div>
       </div>
       <div className="right-scene"></div>
     </div>
